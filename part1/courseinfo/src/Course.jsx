2f4678e209251e2057c3e81bd32501160bdfa1eb
Talha Bay <ihsantalhabay@gmail.com>
2026-08-13 12:44:04 +0300

    part 2a completed

---
+++ part1/courseinfo/src/Course.jsx
@@ -0,0 +1,52 @@
+//part
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+//header
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+//content
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((singlePart) => {
+        return (
+          <Part
+            key={singlePart.id}
+            name={singlePart.name}
+            exercises={singlePart.exercises}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+//total
+const Total = ({ parts }) => {
+  const totalExercises = parts.map((part) => part.exercises);
+  return (
+    <p>
+      <b>Number of exercises {totalExercises.reduce((a, b) => a + b)}</b>
+    </p>
+  );
+};
+
+function Course({ course }) {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+export default Course;
